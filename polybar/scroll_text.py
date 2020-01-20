b6aeaf734588d0c143b0053f4d0c86bd150f7c58
Andrew McCollister <andrewmccollister1995@gmail.com>
2020-01-20 15:56:26 -0600

    Began scrolling text on bar

---
+++ polybar/scroll_text.py
@@ -0,0 +1,17 @@
+#!/bin/python
+
+import time
+import sys
+text = sys.argv[1]
+
+while True:
+    with open(text, "r") as f:
+        output = f.readlines()
+
+    rewrite = output[1:]
+    rewrite.append(output[0])
+
+    with open(text, "w") as f:
+        f.writelines(rewrite)
+    
+    time.sleep(.4)
