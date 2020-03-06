82a8805320f579a297d939a19fc2955e2b9dc1ad
Andrew McCollister <andrewmccollister1995@gmail.com>
2020-03-05 22:17:07 -0600

    updated launched to pick config based on resolution

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,7 +9,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done;
 # Launch polybar
 echo "---" | tee -a /tmp/polybar.log
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar main -r >> /tmp/polybar_$m.log 2>&1 &
+    res=$(polybar --list-monitors | cut -d"x" -f2 | cut -b -4)
+    MONITOR=$m polybar main -r -c ~/.config/polybar/config_$res >> /tmp/polybar_$m.log 2>&1 &
 done
 
 echo "Bars launched bars for all monitors"
