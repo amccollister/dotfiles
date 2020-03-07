4333a2ab69aff9d6cd7fc49be8822672bd1a2fb6
Andrew McCollister <andrewmccollister1995@gmail.com>
2020-03-06 19:34:24 -0600

    fixed launch script for multi-monitor use

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,9 +8,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done;
 
 # Launch polybar
 echo "---" | tee -a /tmp/polybar.log
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    res=$(polybar --list-monitors | cut -d"x" -f2 | cut -b -4)
-    MONITOR=$m polybar main -r -c ~/.config/polybar/config_$res >> /tmp/polybar_$m.log 2>&1 &
+for m in $(polybar --list-monitors | sed 's/ /_/g'); do
+    mon=$(echo $m | cut -d":" -f1)
+    res=$(echo $m | cut -d"x" -f2 | cut -b -4)
+    MONITOR=$mon polybar main -r -c ~/.config/polybar/config_$res >> /tmp/polybar_$m.log 2>&1 &
 done
 
 echo "Bars launched bars for all monitors"
