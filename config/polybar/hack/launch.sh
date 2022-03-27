2acbf7ad1fc99b41b83300d6c497f626b0374c91
Andrew McCollister <andrewmccollister1995@gmail.com>
2022-03-27 14:43:44 -0500

    Adding some delay for polybar launches

--- config/polybar/hack/launch.sh
+++ config/polybar/hack/launch.sh
@@ -6,6 +6,7 @@ DIR="$HOME/.config/polybar/hack"
 
 # Terminate already running bar instances
 killall -q polybar
+sleep 1
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
