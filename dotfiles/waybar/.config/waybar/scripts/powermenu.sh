55271f757c92f3adc1da672068e8a258e47f980c
Andrew McCollister <andrewmccollister1995@gmail.com>
2025-10-18 20:21:39 -0500

    Converting polybar functionality to waybar

---
+++ dotfiles/waybar/.config/waybar/scripts/powermenu.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+dir="~/.config/polybar/hack/scripts/rofi"
+#uptime=$(uptime -p | sed -e 's/up //g')
+#uptime=$(uptime -p | awk '{print $2"h " $4"m"}')
+uptime=$(uptime -p | awk '{print $2 substr($3,1,1) " " $4substr($5,1,1)}')
+
+rofi_command="rofi -theme $dir/powermenu.rasi"
+
+# Options
+shutdown=" Shutdown"
+reboot=" Restart"
+lock=" Lock"
+suspend=" Sleep"
+logout=" Logout"
+windows="  Windows"
+
+# Confirmation
+confirm_exit() {
+	rofi -dmenu\
+		-i\
+		-no-fixed-num-lines\
+		-p "Are You Sure? : "\
+		-theme $dir/confirm.rasi
+}
+
+# Message
+msg() {
+	rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
+}
+
+# Variable passed to rofi
+#options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
+#options="$lock\n$logout\n$reboot\n$shutdown"
+options="$windows\n$logout\n$reboot\n$shutdown"
+
+ans="YES"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
+case $chosen in
+    $shutdown)
+		#ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			systemctl poweroff
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $reboot)
+		#ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			systemctl reboot
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $lock)
+		betterlockscreen -l
+		#if [[ -f /usr/bin/i3lock ]]; then
+		#	i3lock
+		#elif [[ -f /usr/bin/betterlockscreen ]]; then
+		#	betterlockscreen -l
+		#fi
+        ;;
+	$windows)
+		systemctl reboot --boot-loader-entry=windows.conf
+		;;
+    $suspend)
+		#ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			mpc -q pause
+			amixer set Master mute
+			systemctl suspend
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $logout)
+		#ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+				openbox --exit
+			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+				bspc quit
+			elif [[ "$XDG_SESSION_DESKTOP" == "i3" ]]; then
+				i3-msg exit
+			fi
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+esac
