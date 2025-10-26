99d109cf0a78a5c33504080976fe21ca1aa4542d
Andrew McCollister <andrewmccollister1995@gmail.com>
2025-10-26 01:07:46 -0500

    Updated multiwall to check the wallpaper cache

--- dotfiles/hyprland/.config/hypr/multiwall.sh
+++ dotfiles/hyprland/.config/hypr/multiwall.sh
@@ -5,17 +5,20 @@ if [[ -z $1 ]]; then
     echo "Usage: $0 <file>"
     exit 1
 fi
-WALLPAPER="basename $1"
+WALLPAPER=$(basename $1 | cut -d '.' -f 1)
 
 # Determine monitor order
 mapfile -t monitors < <(hyprctl monitors | grep -E '(Monitor |at )' | paste - - | awk '{print $2, $7}' | cut -d'x' -f1 | sort -nk2 | awk '{print $1}')
 
 # Split the image for each monitor
 crop_perc=$((100/${#monitors[@]}))
-magick "$1" -crop $crop_perc%x100% ~/.cache/wallpaper/$WALLPAPER.png
+wp_cache=$(ls -l ~/.cache/wallpaper/$WALLPAPER-* | wc -l)
+if [[ $wp_cache -lt 2 ]]; then
+    magick "$1" -crop $crop_perc%x100% ~/.cache/wallpaper/$WALLPAPER.png
+fi
 
 # Assign the images per monitor
 for i in "${!monitors[@]}"; do
-  swww img -o "${monitors[$i]}" ~/.cache/wallpaper/wall-"$i".png --transition-type left --transition-fps 60; sleep 1.6;
+  swww img -o "${monitors[$i]}" ~/.cache/wallpaper/$WALLPAPER-"$i".png --transition-type left --transition-fps 60; sleep 1.6;
 done
 
