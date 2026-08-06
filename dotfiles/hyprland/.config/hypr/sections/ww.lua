c080dd6093a706b7649e391b2ad7206c0d488ae1
Andrew McCollister <andrewmccollister1995@gmail.com>
2026-08-06 17:23:26 -0500

    Fixing blur and kitty dropdown

--- dotfiles/hyprland/.config/hypr/sections/ww.lua
+++ dotfiles/hyprland/.config/hypr/sections/ww.lua
@@ -7,22 +7,20 @@ hl.window_rule({ match = { class = "vesktop", },  workspace = 5, no_initial_focu
 hl.window_rule({ match = { class = "obsidian", }, workspace = 6, no_initial_focus = true})
 
 -- Kitty Special Workspace Rules
-hl.workspace_rule({ workspace = "special:kitty_drop", on_created_empty = "[float] kitty --session ~/.config/kitty/drop.kitty-session" })
-hl.window_rule({ match = { workspace = "special:kitty_drop", class = "kitty" }, move = { "monitor_w * 0.005", "monitor_h * 0.01" }, size = { "monitor_w * 0.99", "monitor_h * 0.5" } })
-
--- Stopped working for the actual kitty program
--- hl.window_rule({ match = { workspace = "special:kitty_drop", }, workspace = "+0", }) -- don't open programs in the special workspace
+hl.workspace_rule({ workspace = "special:kitty_drop", on_created_empty = "[float] kitty --session ~/.config/kitty/drop.kitty-session --class dropcat" })
+hl.window_rule({ match = { workspace = "special:kitty_drop", class = "dropcat" }, move = { "monitor_w * 0.005", "monitor_h * 0.01" }, size = { "monitor_w * 0.99", "monitor_h * 0.5" } })
+hl.window_rule({ match = { workspace = "special:kitty_drop", class = "negative:dropcat" }, workspace = "e+0", }) -- don't open other programs in the special workspace
 
 -- Flameshot Special Window Rules
-hl.window_rule({ match = { class = "flameshot", title = "flameshot" }, decorate = false, no_anim = true, float = true, pin = true,})
+hl.window_rule({ match = { class = "flameshot", title = "flameshot" }, decorate = false, no_anim = true, float = true, pin = true, move = { 0, 0 } })
 
--- Add blur to specific windows -- ignorealpha doesn't blur is alpha is under value
-hl.layer_rule({match = {namespace = "match:namespace rofi"                       },    blur = true         })
-hl.layer_rule({match = {namespace = "match:namespace rofi"                       },    ignore_alpha = 0.5  })
-hl.layer_rule({match = {namespace = "match:namespace swaync-control-center"      },    blur = true         })
-hl.layer_rule({match = {namespace = "match:namespace swaync-control-center"      },    ignore_alpha = 0.25 })
-hl.layer_rule({match = {namespace = "match:namespace swaync-notification-window" },    blur = true         })
-hl.layer_rule({match = {namespace = "match:namespace swaync-notification-window" },    ignore_alpha = 0.25 })
+-- Add blur to specific windows -- ignorealpha doesn't blur if alpha is under value
+hl.layer_rule({match = {namespace = "rofi"                       },    blur = true         })
+hl.layer_rule({match = {namespace = "rofi"                       },    ignore_alpha = 0.25  })
+hl.layer_rule({match = {namespace = "swaync-control-center"      },    blur = true         })
+hl.layer_rule({match = {namespace = "swaync-control-center"      },    ignore_alpha = 0.25 })
+hl.layer_rule({match = {namespace = "swaync-notification-window" },    blur = true         })
+hl.layer_rule({match = {namespace = "swaync-notification-window" },    ignore_alpha = 0.25 })
 
 -- Ignore maximize requests from all apps. You'll probably like this.
 hl.window_rule({ match = { class = ".*" }, suppress_event = "maximize" })
