76cbd8267df5cf2062ccf77bf2f2dde748925817
Andrew McCollister <andrewmccollister1995@gmail.com>
2026-08-06 17:49:01 -0500

    Two display files for different monitor sizes

--- dotfiles/hyprland/.config/hypr/hyprland.lua
+++ dotfiles/hyprland/.config/hypr/hyprland.lua
@@ -1,7 +1,17 @@
 require("~/.cache/wal/colors-hyprland.lua")
 
--- TODO: Change display based on your monitors
-require("sections/display")
+-- Change display based on monitor size
+local resolution = 0
+for _, mon in ipairs(hl.get_monitors()) do
+    resolution = math.max(resolution, mon.height)
+end
+
+if resolution == 1440 then
+    require("sections/display_1440")
+elseif resolution == 1080 then
+    require("sections/display_1080")
+end
+
 require("sections/env")
 require("sections/appearance")
 require("sections/controls")
